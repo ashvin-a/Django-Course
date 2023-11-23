e5ce126c10c7acb9f69a278139b6c3d3d5dd66f9
Ashvin <ashvin.anilkumar@qburst.com>
2023-11-23 14:47:15 +0530

    less noob aane

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:month>",views.month_int),
-    path("<str:month>",views.month_response)
+    path("<month>",views.month_res)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound
 # Create your views here.
 
-def month_response(request,month):
-    text = ""
-    if month=="january":
-        text = "To a new beginning!"
-    elif month=="february":
-        text = "Only 29 days for this month!"
-    elif month=="march":
-        text = "Summer is around the corner"
-    else:
-        return HttpResponseNotFound("Ithu ninte area alla!")
+month_response = {
+    "january":"To a new beginning",
+    "february":"Only 28 days in this month",
+    "march":"Summer is around the corner",
+    "april":"Fools day",
+    "may":"May Day Holiday",
+    "june":"Meh this is getting boring",
+    "july":"Yeah its definetly getting boring",
+    "august":"Leo was born",
+    "september":"Its about damn time we take on a challenge",
+    "october":"Lets hit gym all day",
+    "november":"#NNN",
+    "december":"Jingle bells are on the way"
+}
+
+def month_res(request,month):
+    try:
+        text = month_response[month]
+    except:
+        return HttpResponseNotFound("Ithu ninte area allaa!!")
     return HttpResponse(text)
 
-def month_int(request,month):
-    return HttpResponse(month)
