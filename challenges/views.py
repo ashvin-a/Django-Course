36c5b3715992c5fa18484b4e0e9a3da95cbc74fc
Ashvin <ashvin.anilkumar@qburst.com>
2023-11-23 14:08:40 +0530

    noob method

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def january(request):
+    return HttpResponse("To a new beginning!")
+
+def february(request):
+    return HttpResponse('Only 29 days for this month!')
+
+def march(request):
+    return HttpResponse('Summer is around the corner!')
